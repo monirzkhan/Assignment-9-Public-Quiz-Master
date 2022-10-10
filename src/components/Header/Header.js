22834bc60d2678afd39159fa7699b1a451872f39
monirzkhan <odvutkabbo@gmail.com>
2022-10-11 02:03:18 +0600

    Header responsive design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css'
 import logo from '../../images/logo.png'
+import { Bars3BottomLeftIcon, Bars3Icon, BeakerIcon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <div className='flex items-center justify-between bg-yellow-400 p-4'>
+        <div className='flex items-center justify-between bg-yellow-400 p-4 shadow'>
+
+            <div className='h-10 w-10 md:hidden' onClick={()=>setOpen(!open)}>
+                {
+                open?<XMarkIcon />: <Bars3Icon />
+                }
+            </div>
+
             <div className='flex items-center mx-auto'>
                 <img className='w-[48px] h-[48px]' src={logo} alt="" />
-                <h1 className='font-mono text-2xl'>Brain Storm</h1>
+                <h1 className='font-mono text-2xl ml-1'>Brain Storm</h1>
             </div>
-            <div className='header mx-auto'>
+            <div className={`header bg-yellow-100 md:bg-yellow-400  md:mx-auto absolute duration-700 ease-in md:static ${open ? 'top-20': 'top-[-150px]'}`}>
                 <NavLink to='home'>Home</NavLink>
                 <NavLink to='chart'>Statistic</NavLink>
                 <NavLink to='blog'>Blog</NavLink>
